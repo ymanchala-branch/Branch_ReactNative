59901f5c1e0abed6a3afa6d26b6d6b034d2c662a
yashwanthi-m <91668777+yashwanthi-m@users.noreply.github.com>
2024-09-04 22:46:23 +0530

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState , useEffect} from 'react';
+import branch from 'react-native-branch';
+import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -15,6 +9,8 @@ import {
   Text,
   useColorScheme,
   View,
+  TouchableOpacity,
+  Alert,
 } from 'react-native';
 
 import {
@@ -23,13 +19,16 @@ import {
   Header,
   LearnMoreLinks,
   ReloadInstructions,
+  
 } from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
+
+
+function Section({ children, title }: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -57,49 +56,122 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
+  const urls = [
+    { "branch": "https://yash.devishetty.net/WDSFEy6vAMb" },
+    { "branch": "https://yash.devishetty.net/e/baWd1jtcsLb" },
+    { "branch": "https://yash.devishetty.net/GYFvXLVIBJb" },
+    { "branch": "https://yash.devishetty.net/vfiR8v7ndJb" },
+    { "branch": "https://yash.devishetty.net/XmDH7b5ndJb" }
+  ];
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [selectedUrl, setSelectedUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    console.log("Test test34566")
+    branch.subscribe({
+      onOpenStart: ({
+          uri,
+          cachedInitialEvent
+      }) => {
+          console.log(
+              'subscribe onOpenStart, will open ' +
+              uri +
+              ' cachedInitialEvent is ' +
+              cachedInitialEvent,
+          );
+      },
+      onOpenComplete: ({
+          error,
+          params,
+          uri
+      }) => {
+          if (error) {
+              console.error(
+                  'subscribe onOpenComplete, Error from opening uri: ' +
+                  uri +
+                  ' error: ' +
+                  error,
+              );
+              return;
+          }
+          else if (params) {
+              if (!params['+clicked_branch_link']) {
+                  if (params['+non_branch_link']) {
+                      console.log('non_branch_link: ' + uri);
+                      // Route based on non-Branch links
+                      return;
+                  }
+              } else {
+                  // Handle params
+                  let deepLinkPath = params.$deeplink_path as string; 
+                  let canonicalUrl = params.$canonical_url as string;
+                  const paramsString = JSON.stringify(params, null, 2); // Convert params to a formatted JSON string
+                  Alert.alert('Branch Params', paramsString);
+                  // Route based on Branch link data 
+                  return
+              }
+          }
+      },
+  });
+  }, []);
+
+
+  const handlePress = async (url: string) => {  
+    try {
+      setSelectedUrl(url); // Update selected URL to the string value
+      if (branch) {
+        await branch.openURL(url); // Open the Branch URL
+      } else {
+        console.error('Branch SDK is not initialized');
+      }
+    } catch (error) {
+      console.error('Error opening URL:', error);
+    }
+  };
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      style={{ flex: 1 }}>
+      <View style={{ padding: 20 }}>
+        {/* Map through the urls array to dynamically create buttons */}
+        {urls.map((urlObj, index) => (
+          <View key={index} style={{ marginBottom: 20 }}>
+            <Text style={{ fontSize: 14 }}>Link: {urlObj.branch}</Text>
+            <TouchableOpacity
+              style={{ backgroundColor: 'grey', padding: 10, marginTop: 10, borderRadius: 10}}
+              onPress={() => handlePress(urlObj.branch)}>
+              <Text style={{ color: '#fff', fontSize: 16 }}>Click</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+        {/* Display the selected URL */}
+        {selectedUrl && (
+          <View style={{ marginTop: 20 }}>
+            <Text style={{ fontSize: 16, color: 'black', fontWeight: 'bold' }}>Selected URL: {selectedUrl}</Text>
+          </View>
+        )}
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
+    marginTop: 300,
     paddingHorizontal: 24,
+  },
+  urlText: {
+    fontSize: 14,
+    marginBottom: 20,
+  },
+  container: {
+    flex: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   sectionTitle: {
     fontSize: 24,
@@ -112,6 +184,50 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  button: {
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    backgroundColor: '#007AFF',
+    borderRadius: 5,
+    marginTop: 20,
+    alignItems: 'center', // Adds space above the button
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    alignItems: 'center',
+  },
+  centeredView: {
+    flex: 1, // Take up the full height
+    justifyContent: 'center', // Center content vertically
+    alignItems: 'center', // Center content horizontally
+  },
+  paramsContainer: {
+    marginTop: 20,
+    padding: 20,
+    backgroundColor: '#e0e0e0', // Light gray background for the rectangle
+    borderRadius: 5,
+    width: '100%', // Full width of the container
+    alignItems: 'center', // Center text horizontally
+  },
+  paramsText: {
+    fontSize: 16,
+    color: '#333', // Dark text color
+  },
+  header: {
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#007AFF',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  headerTitle: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
--- ios/Branch_Test_RN_NC/AppDelegate.mm
+++ ios/Branch_Test_RN_NC/AppDelegate.mm
@@ -1,16 +1,49 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <RNBranch/RNBranch.h>
+#import <Firebase.h>
+#import <FirebaseMessaging/FirebaseMessaging.h>
+#import <UserNotifications/UserNotifications.h>
+
+@interface AppDelegate () <UNUserNotificationCenterDelegate>
+@end
+
+@interface AppDelegate () <FIRMessagingDelegate>
+@end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [application registerForRemoteNotifications];
+  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+  UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
+      UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+  [[UNUserNotificationCenter currentNotificationCenter]
+      requestAuthorizationWithOptions:authOptions
+      completionHandler:^(BOOL granted, NSError * _Nullable error) {
+      }];
+  
+  [FIRApp configure];
+  [FIRMessaging messaging].delegate = self;
+  [RNBranch enableLogging];
+  // Add Snap partner parameters
+  [[Branch getInstance] addSnapPartnerParameterWithName:@"hashed_email_address"
+                                           value:@"11234e56af071e9c79927651156bd7a10bca8ac34672aba121056e2698ee7088"];
+  [[Branch getInstance] addSnapPartnerParameterWithName:@"hashed_phone_number"
+                                           value:@"534a4a8eafcd8489af32356d5a7a25f88c70cfe0448539a7c42964c1b897a359"];
+  [RNBranch initSessionWithLaunchOptions:launchOptions isReferrable:YES];
+ 
   self.moduleName = @"Branch_Test_RN_NC";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  
 
+   NSURL *jsCodeLocation;
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
@@ -26,6 +59,31 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  [RNBranch application:app openURL:url options:options];
+  return YES;
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+  [RNBranch continueUserActivity:userActivity];
+  return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    // Pass device token to Firebase
+  [FIRMessaging messaging].APNSToken = deviceToken;
+  [[FIRMessaging messaging] setAPNSToken:deviceToken];
+}
+
+- (void)messaging:(FIRMessaging *)messaging didReceiveRegistrationToken:(NSString *)fcmToken {
+    NSLog(@"FCM registration token: %@", fcmToken);
+    // Notify about received token.
+    NSDictionary *dataDict = [NSDictionary dictionaryWithObject:fcmToken forKey:@"token"];
+    [[NSNotificationCenter defaultCenter] postNotificationName:
+     @"FCMToken" object:nil userInfo:dataDict];
+    // TODO: If necessary send token to application server.
+    // Note: This callback is fired at each app startup and whenever a new token is generated.
 }
 
 @end
